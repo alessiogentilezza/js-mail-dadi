99db6cfc38fec48cab069f532a0e914c06a9a5e1
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-02-27 16:30:14 +0100

    aggiornato codice con l'utilizzio di una variabile booleana

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,12 +1,16 @@
 const emailArray = ['tizio@gmail.com', 'caio@hotmail.com', 'sempronio@yahoo.com'];
 const userEmail = prompt("Inserisci il tuo indirizzo email:");
 
+let checkMail = false;
+
 for (let i = 0; i < emailArray.length; i++) {
     if (emailArray[i] == userEmail) {
-        console.log(`L'indirizzo email ${userEmail} è presente nell'array.`);
-        break;
-
-    } else {
-        console.log(`L'indirizzo email ${userEmail} non è presente nell'array.`);
+        checkMail = true;
     }
+}
+
+if (checkMail) {
+    console.log(`L'indirizzo email ${userEmail} è presente nell'array.`);
+} else {
+    console.log(`L'indirizzo email ${userEmail} non è presente nell'array.`);
 }
